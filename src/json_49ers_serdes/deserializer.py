1c6cc4f8e6bc73b7b288a4d582fc96deba1e1680
Jamie Mortensen <16mortensenj@gmail.com>
2022-10-23 16:10:50 -0400

    began implementing deserializer

--- src/json_49ers_serdes/deserializer.py
+++ src/json_49ers_serdes/deserializer.py
@@ -2,13 +2,21 @@
 from __future__ import annotations
 from pathlib import Path
 
+from src.data.game import Game
+import json
+
 
 class Deserializer:
     def __init__(self):
         pass
 
-    def from_string(self, string: str):
+    def from_string(self, json_string: str) -> list[Game]:
+        data = json.loads(json_string)
         pass
 
-    def from_file(self, path: Path):
-        pass
+    def from_file(self, path: Path) -> list[Game]:
+        json_str = ""
+        with open(path, "r") as fp:
+            json_str = fp.read()
+
+        return self.from_string(json_str)
