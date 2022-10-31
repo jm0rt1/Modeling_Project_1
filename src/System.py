0205034303ecc9dbcba548200286a3cc3e0c26a2
Jamie Mortensen <16mortensenj@gmail.com>
2022-10-30 22:41:39 -0400

    implemented load_data and save_data

--- src/System.py
+++ src/System.py
@@ -1,21 +1,58 @@
 
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+from typing import Optional
+
 from src.data.game import Game
+from src.json_49ers_serdes.deserializer import Deserializer
+from src.json_49ers_serdes.serializer import Serializer
 
 
 class System:
     def __init__(self):
-        pass
+        self.games: Optional[list[Game]] = None
 
     def load_data(self, ):
-        pass
+        path = choose_file_open()
+        self.games = Deserializer().from_file(path)
+        print(f"{len(self.games)} games loaded")
 
     def save_data(self, ):
-        pass
+        if self.games is None:
+            print("No Games Loaded...")
+            return
+
+        path = choose_file_save()
+        Serializer().to_file(self.games, path)
+        print(f"{len(self.games)} have been saved to {path.as_posix()}")
 
     def add_game(self, game: Game):
         pass
 
     def Operation1(self, ):
         pass
+
+
+def choose_file_open():
+    import tkinter as tk
+    from tkinter import filedialog
+
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()
+    root.quit()
+    return Path(file_path)
+
+
+def choose_file_save():
+    import tkinter as tk
+    from tkinter import filedialog
+
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.asksaveasfilename()
+    root.quit()
+    return Path(file_path)
