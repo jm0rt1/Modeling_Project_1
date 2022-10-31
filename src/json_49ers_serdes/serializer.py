c745f03b9b08f2efe46078f42e2385b4aa20a366
Jamie Mortensen <16mortensenj@gmail.com>
2022-10-30 22:01:28 -0400

    fixed issue with the reference being modified using deepcopy

--- src/json_49ers_serdes/serializer.py
+++ src/json_49ers_serdes/serializer.py
@@ -1,5 +1,6 @@
 
 from __future__ import annotations
+from copy import copy, deepcopy
 import json
 from pathlib import Path
 from src.data.game import Game
@@ -10,12 +11,15 @@ class Serializer:
         pass
 
     def to_string(self, games: list[Game]):
+        # make a copy, so as not to bugger the ref
+        g = deepcopy(games)
         games_dicts_list = []
-        for game in games:
+
+        for game in g:
             game_dict = game.to_dict()
             games_dicts_list.append(game_dict)
 
-        string = json.dumps(games)
+        string = json.dumps(games_dicts_list)
 
         return string
 
