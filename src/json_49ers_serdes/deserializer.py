4f95fe3d6ce2e3f78df51baef3b1017b1fa56e66
Jamie Mortensen <16mortensenj@gmail.com>
2022-11-13 08:57:55 -0500

    ignored typing problems

--- src/json_49ers_serdes/deserializer.py
+++ src/json_49ers_serdes/deserializer.py
@@ -13,10 +13,12 @@ class Deserializer:
     def from_string(self, json_string: str) -> list[Game]:
         data = json.loads(json_string)
         games: list[Game] = []
-        if "matchUpStats" in data.keys():
-            data = data["matchupStats"]
-        for item in data:
-            game = Game.from_dict(item)
+        if isinstance(data, dict) and "matchUpStats" in data.keys():
+            data = data["matchupStats"]  # type:ignore
+        if not (isinstance(data, list)):
+            RuntimeError("Data did not reduce to a list[dict[Any,Any]]")
+        for item in data:  # type:ignore
+            game = Game.from_dict(item)  # type:ignore
             games.append(game)
         return games
 
