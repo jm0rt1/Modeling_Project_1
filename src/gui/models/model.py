2f4d674c811ae9e1f8633120496d0a5e26bb7edd
Jamie Mortensen <16mortensenj@gmail.com>
2022-12-11 17:32:18 -0500

    formatting

--- src/gui/models/model.py
+++ src/gui/models/model.py
@@ -11,7 +11,6 @@ class Model(QtCore.QObject):
         super().__init__(parent)
         self.games_combobox_model = GamesListComboboxModel()
         self.displayed_game = GameDisplayTableModel()
-        
 
     def load_data_from_snoozle_server(self, year: int, name: str):
         self.games = SnoozelSportsClient.get_team_stats_by_name(
